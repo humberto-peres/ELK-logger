b32356e8c25d0a01883b4f2c6ebfe5168355eac0
humberto-peres <betinhoperes.filho@gmail.com>
2024-08-18 19:21:22 -0300

    Geração de logs com elasticsearch

---
+++ spring-log-elk/src/test/java/com/logs/spring_log_elk/SpringLogElkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logs.spring_log_elk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLogElkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
